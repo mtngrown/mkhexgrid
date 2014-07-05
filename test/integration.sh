cf831ed330c1bfc563bee1447786d202f4a9d727
Juan Valdez <mtngrown7@yahoo.com>
2014-07-05 13:13:52 -0700

    started simple integration test for sanity

---
+++ test/integration.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source ./bashcolors.sh
+initializeANSI
+
+echo
+echo $cyanf"# Integration testing for mkhexgrid"
+echo
+
+source ./svg_1x1.sh
+
+function test_svg() {
+
+  results=`diff $1 $2`
+  if [[ $results ]] ; then
+    echo $redf"failed $1: diff results in $results"
+  else
+    echo $greenf"passed $1"
+  fi
+}
+
+
+control="./control/control_1x1.svg"
+output="./output/output_1x1.svg"
+test_svg $control $output
+
+echo
---
+++ test/svg_1x1.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+../mkhexgrid  --hex-width=100 --columns=1 --rows=1 --coord-size=8 --grid-grain=h\
+  --image-margin=50 --coord-origin=ur -o output/output_1x1.svg --output=svg
