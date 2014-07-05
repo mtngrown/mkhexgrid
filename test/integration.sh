7a1518768f1ed857fd64685c65cfe4dbd311e771
Juan Valdez <mtngrown7@yahoo.com>
2014-07-05 13:55:23 -0700

    integration testing for refactoring center defs

--- test/integration.sh
+++ test/integration.sh
@@ -11,6 +11,8 @@ source ./svg_1x1.sh
 
 function test_svg() {
 
+
+
   results=`diff $1 $2`
   if [[ $results ]] ; then
     echo $redf"failed $1: diff results in $results"
@@ -21,7 +23,15 @@ function test_svg() {
 
 
 control="./fixtures/1x1.svg"
-output="./output/output_1x1.svg"
+output="./output/1x1.svg"
+test_svg $control $output
+
+control="./fixtures/6x6_center_cross.svg"
+output="./output/6x6_center_cross.svg"
+test_svg $control $output
+
+control="./fixtures/8x8_center_dot.svg"
+output="./output/8x8_center_dot.svg"
 test_svg $control $output
 
 echo
---
+++ test/svg.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+../mkhexgrid  --hex-width=100 --columns=1 --rows=1 --coord-size=8 --grid-grain=h\
+  --image-margin=50 --coord-origin=ur -o output/1x1.svg --output=svg
+
+../mkhexgrid  --hex-width=100 --columns=6 --rows=6 --coord-size=10 --grid-grain=h\
+  --image-margin=50 --coord-origin=ur -o output/6x6_center_cross.svg --output=svg --center-style=c
+
+../mkhexgrid  --hex-width=100 --columns=8 --rows=8 --coord-size=8 --grid-grain=h\
+  --image-margin=50 --coord-origin=ur -o output/8x8_center_dot.svg --output=svg --center-style=d
